d05520c3007320ecc46e2fc98911f72f882858a2
dilli <dillihangrae@gmail.com>
2022-03-08 21:47:08 +0545

    added

---
+++ index.js
@@ -0,0 +1,6 @@
+function Welcome(greeting) {
+    console.log(greeting);
+}
+var greeting = "hello world";
+Welcome(greeting);
+// always use node index.js for terminal js run
---
+++ index.ts
@@ -0,0 +1,15 @@
+
+function CustomWelcome(greeting){
+    console.log(greeting)
+}
+
+var greeting = "hello world";
+CustomWelcome(greeting);
+
+
+let i = 0;
+for (i; i<5; i++){
+    console.log(i)
+}
+
+// always use node index.js for terminal js run
