cf95ee77e3226a246ddf297178c120c37bec09db
dilli <dillihangrae@gmail.com>
2022-04-12 19:00:42 +0545

    [added] adds ant design and ant design pro with ts managed by yarn....

---
+++ antd-demo-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './antd-UI/css/admin/root.css';
+import AdminLayout from './antd-UI/admin/layout';
+
+function App() {
+  return (
+    <div>
+      <AdminLayout/>
+    </div>
+  );
+}
+
+export default App;
