ec3845d1d35889f12130cc4547144499910d9b35
dilli <dillihangrae@gmail.com>
2024-06-03 16:36:23 +0545

    Day3 of 60DaysOfLearning

--- 2024~60DaysOfLearning/Day3/day3.py
+++ 2024~60DaysOfLearning/Day3/day3.py
@@ -1,10 +1,8 @@
 # In Python, __init__ is a special method known as the constructor.
 # It is automatically called when a new instance of a class is created. 
 # The purpose of __init__ is to initialize the attributes of the class. 
-
 # classes are the blueprint of the object. It defines the properties (attributes) 
 # and functionalities (methods) that objects of that class will have.
-
 # self --> a clear and consistent way to refer to instance variables and methods within class definitions.
 # Instance Reference,Attribute Access and It is explicitly included in the method definitions, 
 # unlike languages like C++ or Java, but in python where the instance reference is implicit.
@@ -23,17 +21,14 @@ class Car:
   def brake(self):
     print("The car is braking!")
 
-# # Create objects (instances) of the Car class
-# toyota_corolla = Car("Toyota", "Corolla", "Silver")
-# tesla_model_s = Car("Tesla", "Model S", "Black")
-
-# # Accessing attributes and methods
-# print(toyota_corolla.make)  # Output: Toyota
-# print(tesla_model_s.color)  # Output: Black
-
-# toyota_corolla.accelerate()  # Output: The car is accelerating!
-# tesla_model_s.brake()  # Output: The car is braking!
-
+# Create objects (instances) of the Car class
+toyota_corolla = Car("Toyota", "Corolla", "Silver")
+tesla_model_s = Car("Tesla", "Model S", "Black")
+# Accessing attributes and methods
+print(toyota_corolla.make)  # Output: Toyota
+print(tesla_model_s.color)  # Output: Black
+toyota_corolla.accelerate()  # Output: The car is accelerating!
+tesla_model_s.brake()  # Output: The car is braking!
 
 class Model:
     def __init__(self, modelName, algorithms):
@@ -58,10 +53,8 @@ class Model:
         Prints the list of algorithms used by the model to the console. # set return
         """
         print(f"The algorithms used are: {self.algorithms}")  # Using f-string for clarity
-
 # Creating object
 obj1 = Model("LM", ["GPT", "Transformer"])  # Create a model with multiple algorithms
-
 # Calling methods (without the parentheses)
 print(obj1.announceModelName())  # Call the method to print the model name
 print(obj1.modelAlgo())  # Call the method to print the algorithms
